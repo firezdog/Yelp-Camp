3608876c70222916ff3952f89f677533a5f3a12e
firezdog@yahoo.com <firezdog@yahoo.com>
2018-03-02 15:22:43 -0600

    Updated to allow persistent data...

--- app.js
+++ app.js
@@ -1,36 +1,99 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose =   require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("campground", campgroundSchema);
+
+// Campground.create (
+//     {
+//         name: "Salmon Creek",
+//         image: "https://acadiamagic.com/280x187/md-campground.jpg"
+//     }, 
+//     function(err, campground) {
+//         if(err) {
+//             console.log(err);
+//         }
+//         else {
+//             console.log("NEWLY CREATED CAMPGROUND");
+//             console.log(campground);
+//         }
+//     }
+// );
+
+// Campground.create (
+//     {
+//         name: "Geneva Glen",
+//         image: "https://www.marylmartin.com/wp-content/uploads/2016/01/343591.jpg"
+//     },
+//     function(err, campground) {
+//         if(err) {
+//             console.log(err);
+//         }
+//         else {
+//             console.log("NEWLY CREATED CAMPGROUND");
+//             console.log(campground);
+//         }
+//     }
+// );
+
+// Campground.create (
+//     {
+//         name: "Erana's Peace",
+//         image: "https://i.pinimg.com/originals/b3/66/17/b3661708c29006930fd15fb963864e39.jpg"
+//     },
+//     function(err, campground) {
+//         if(err) {
+//             console.log(err);
+//         }
+//         else {
+//             console.log("NEWLY CREATED CAMPGROUND");
+//             console.log(campground);
+//         }
+//     }
+// )
 
 app.get("/", function(req, res) {
     res.render("landing")
 });
 
-var campgrounds = [
-    {
-        name: "Salmon Creek",
-        image: "https://acadiamagic.com/280x187/md-campground.jpg"
-    },
-    {
-        name: "Geneva Glen",
-        image: "https://www.marylmartin.com/wp-content/uploads/2016/01/343591.jpg"
-    },
-    {
-        name: "Erana's Peace",
-        image: "https://i.pinimg.com/originals/b3/66/17/b3661708c29006930fd15fb963864e39.jpg"
-    },
-]
-
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, campgrounds){
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    })
 });
 
 app.post("/campgrounds", function(req, res) {
-    campgrounds.push({name: req.body.name, image: req.body.image})
-    res.redirect("/campgrounds");
+    Campground.create(
+        {
+            name: req.body.name, 
+            image: req.body.image
+        }, 
+        function(err, campground) {
+        
+            if(err) {
+                console.log(err);
+            }
+            else {
+                res.redirect("/campgrounds");
+            }
+        }
+    )    
 });
 
 app.get("/campgrounds/new", function(req, res) {
